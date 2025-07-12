c983d4619253f20dd3d921106cdb24f2f2ace960
HerSebGar <hsebastian.garcia@ucuenca.edu.ec>
2025-07-12 12:39:59 -0500

    Modificacion: agregar jwt requiered

--- app/routes/ImageRoute.py
+++ app/routes/ImageRoute.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Blueprint,send_from_directory
 import os
 import io
+from flask_jwt_extended import jwt_required
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
@@ -9,9 +10,11 @@ from app.controller.ImageController import ImageController
 image_routes = Blueprint('image', __name__)
 
 @image_routes.route('/upload-image/<int:id>', method=["POST"])
+@jwt_required()
 def upload(id):
     return ImageController.UploadImage(id)
 
 @image_routes.route('/uploads/<filename>')
+@jwt_required()
 def uploaded_file(filename):
     return send_from_directory('uploads', filename)
--- app/routes/UserNotificationRoutes.py
+++ app/routes/UserNotificationRoutes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, request, jsonify, session, current_app
 from app.controller.LostModeController import LostModeController
 from app.controller.NotifyUserController import NotifyUserController
+from flask_jwt_extended import jwt_required
+
 notify = Blueprint('notify', __name__)
 
 @notify.route('/lost-mode/<int:id>', methods = ['GET'])
+@jwt_required()
 def lostMode(id):
     return LostModeController.activate(id)
 
 @notify.route('/notify-acces/<int:id>', methods = ['GET'])
+@jwt_required()
 def notifyAcces(id):
     return NotifyUserController.NotifyInformationAccess(id)
