1bf09371dd24ae340b6d67c3cf3956535499ae76
HerSebGar <hsebastian.garcia@ucuenca.edu.ec>
2025-07-16 22:56:10 -0500

    modificacion: Realizando cambios a sw y manifest

--- app/static/js/aplicacionMovil/cuerpo.js
+++ app/static/js/aplicacionMovil/cuerpo.js
@@ -86,6 +86,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuUsuario = document.getElementById("menuUsuario");
     const botonAlarma = document.getElementById("botonEmergencia"); 
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('../../sw.js')
+        .then(reg => console.log("✅ Service Worker registrado", reg))
+        .catch(err => console.error("❌ Error al registrar SW", err));
+    }
+
+
     accionBotonAlarma(botonAlarma);
     abrirContenedores(opciones);
     cerrarContenedor(botonesBajar);
